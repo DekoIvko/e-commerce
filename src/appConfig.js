9f7b88ac703eb2c38dca7a0a64d192354feeda01
DekoIvko <ivkovski89@hotmail.com>
2023-12-12 14:59:01 +0100

    remove files

--- src/appConfig.js
+++
@@ -1,4 +0,0 @@
-export const appConfig = {
-  environment: process.env.REACT_APP_ENVIRONMENT,
-  baseApiURL: "http://localhost:8000",
-};
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header/Header";
-export { default as HomeCart } from "./HomeCart/HomeCart";
-export { default as CartFeature } from "./CartFeature/CartFeature";
-export { default as FilterProducts } from "./FilterProducts/FilterProducts";
-export { default as AllProducts } from "./AllProducts/AllProducts";
-export { default as CartProduct } from "./CartProduct/CartProduct";
